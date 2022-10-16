e7e5124fa847e409bcfb624889e69af19f19e34e
darrenanimo <72428819+darrenanimo@users.noreply.github.com>
2022-10-15 21:33:30 -0700

    9:30pm

--- src/components/topribbonhome.jsx
+++ src/components/topribbonhome.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './topribbon.css'
+import logo from "../images/lightly-logo.png";
 
 function TopRibbonHome() {
     return (
       <div id="topribbon">
         
           <div class="navleft">
-            <Link to="/dashboard"><button class="logo">lightly</button></Link>
+          <Link to="/dashboard"><img src={logo} id="logo"/></Link>
             
           </div>
-          <div class = "navright">
-          <p><Link to="/resources"> <button class="nav-links">Resources </button></Link></p>
-            <p><Link to="/calendar"> <button class="nav-links">Personal Planning </button></Link></p>
+          <div class = "navhome">
             <p><Link to="/dashboard"> <button class="nav-links">Sign In </button> </Link></p>
           </div>
       </div>
